e4be8d0140cb83272e7332614c32977917a2cd55
haythem mattoussi <mattoussihaythem@gmail.com>
2024-10-10 18:36:24 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carwashapp',
+  webDir: 'www'
+};
+
+export default config;
